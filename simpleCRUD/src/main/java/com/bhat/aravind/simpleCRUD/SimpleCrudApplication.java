e6b2753b3dc8238c25a89adf68f6c42c043662f5
aravind <aravindcbk@gmail.com>
2025-08-16 22:39:52 +0530

    trying to run

--- simpleCRUD/src/main/java/com/bhat/aravind/simpleCRUD/SimpleCrudApplication.java
+++ simpleCRUD/src/main/java/com/bhat/aravind/simpleCRUD/SimpleCrudApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleCrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleCrudApplication.class, args);
+
+        SpringApplication.run(SimpleCrudApplication.class, args);
 	}
 
 }
