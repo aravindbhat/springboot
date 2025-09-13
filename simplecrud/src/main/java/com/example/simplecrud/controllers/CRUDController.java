fe17485c636527f3abe85aa82a01c50544f12516
aravind <aravindcbk@gmail.com>
2025-09-13 18:55:44 +0530

    Adding crossorigin

--- simplecrud/src/main/java/com/example/simplecrud/controllers/CRUDController.java
+++ simplecrud/src/main/java/com/example/simplecrud/controllers/CRUDController.java
@@ -4,11 +4,9 @@ import com.example.simplecrud.Entity.Clans;
 import com.example.simplecrud.repository.SimpleCrudRepository;
 import com.example.simplecrud.service.SimpleCrudService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class CRUDController {
     @Autowired
